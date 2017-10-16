4fd78156fdfa26401cbfb57e07862b1a8a702cf4
brickatyourfeet <brickatyourfeet@gmail.com>
2017-10-15 21:35:02 -0700

    added stuff

--- app.js
+++ app.js
@@ -2,5 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const listener = () => console.log(`Application listening on port ${port}!`)
+const listener = () => console.log(`Partyin on port ${port}!`)
 app.listen(port, listener)
+
+
+app.get('/ping', (req, res) => {
+  const message = 'pong!'
+  console.log(`ON THE SERVER: ${message}`)
+  res.send(`RESPONDING TO THE CLIENT: ${message}`)
+})
+
+console.log(typeof app.listen)
